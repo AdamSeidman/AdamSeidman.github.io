4fd74f03e38ef5eecb9ec8c941d63b0b1544f5f5
Adam Seidman <adamjseidman@gmail.com>
2020-08-03 21:56:39 -0400

    Roughed out Project Panel

---
+++ src/components/ProjectPanel.vue
@@ -0,0 +1,59 @@
+<template>
+    <div class="project-panel">
+        <h3>{{ this.projectName }}</h3>
+        <div class="lower-project-content">
+            <img :src="imageFile" />
+        </div>
+        <p>{{ this.projectText }}</p>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        projectName: String,
+        imgName: String,
+        projectText: String
+    },
+    computed: {
+        imageFile: function () {
+            const url = require(`@/assets/${this.imgName}`)
+            return url
+        }
+    }
+}
+</script>
+
+<style scoped>
+.project-panel {
+    display: inline-block;
+    width: 25vw;
+    height: 25vh;
+    border: inset;
+    background-color: #444444;
+    margin: 2vh;
+    transition: margin-top 0.5s,
+        margin-bottom 0.5s,
+        margin-left 0.25s,
+        margin-right 0.25s;
+    padding: 0.75vh;
+}
+
+.project-panel:hover {
+    margin-top: 0.25vh;
+    margin-bottom: 3.75vh;
+    margin: 0.25vh 1vh 3.75vh 3vh;
+    transition: margin-top 0.5s,
+        margin-bottom 0.5s,
+        margin-left 0.25s,
+        margin-right 0.25s;
+    cursor: pointer;
+}
+
+img {
+    display: inline-block;
+    width: 9vw;
+    margin-left: 1vw;
+    clip-path: fill-box;
+}
+</style>
--- src/views/MyProjects.vue
+++ src/views/MyProjects.vue
@@ -1,13 +1,22 @@
 <template>
-  <div>projects</div>
+  <div class="project-container">
+    <project-panel imgName="adam_seidman.jpg" :projectName="'TEST Project'" :projectText="'Yo yo what up my name jeff'" />
+  </div>
 </template>
 
 <script>
-export default {
+import ProjectPanel from '../components/ProjectPanel'
 
+export default {
+  components: {
+    ProjectPanel
+  }
 }
 </script>
 
-<style>
-
+<style scoped>
+div.project-container {
+  margin: 7vh;
+  position: absolute;
+}
 </style>
