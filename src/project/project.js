468d3fabe1aa8ee59b05b7203fe107870b99837e
Adam Seidman <adamjseidman@gmail.com>
2020-08-05 12:31:19 -0400

    Added Pictures

--- src/project/project.js
+++ src/project/project.js
@@ -15,12 +15,12 @@ const projects = [
         'Cloud 18 is a band run by students located at the University of Albany. With input from the band, I designed the website. It is a simple site, but is moderately CSS-intensive.', 
         [
             'Bootstrap', 'CSS', 'Github-Pages'
-        ], 'adam_seidman.jpg', 0),
+        ], 'cloud_18_logo.png', 0),
     new Project('DSF Bot', '',
         'DSF Bot is a Discord robot that has many commands to generate random, but obvious facts. The concept of this project may be funny, but the implementation is no joke!',
         [
             'Discord.JS', 'NodeJS', 'ServeStatic', 'SQLite'
-        ], 'adam_seidman.jpg', 1),
+        ], 'dsf_logo.png', 1),
     new Project('MR System Controller', '',
         'Every year, RIT Baja builds a new vehicle while trying to stay innovative. In the last few years, we have developed an electronic shock absorber system. I designed a custom PCB for this project.',
         [
@@ -40,7 +40,7 @@ const projects = [
         'RIT Baja\'s previous website had a very old look to it. I collaborated on this project with one other student, and we went for a simple, modern one-page design.',
         [
             'AOS', 'Bootstrap', 'Instagram API'
-        ], 'adam_seidman.jpg', 5)
+        ], 'baja_logo.png', 5)
 ]
 
 export default projects
