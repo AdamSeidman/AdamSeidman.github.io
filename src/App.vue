cdd1d4935ef11182ead13be4f3e4d657855eec51
Adam Seidman <adamjseidman@gmail.com>
2020-08-14 17:37:52 -0400

    Testing Style Aspects

--- src/App.vue
+++ src/App.vue
@@ -10,11 +10,27 @@
 import MediaBar from './components/MediaBar'
 import NavBar from './components/NavBar'
 
+const DEBUG = false
+
 export default {
   name: 'app',
   components: {
     MediaBar,
     NavBar
+  },
+  created () {
+    window.addEventListener("resize", this.handleResize)
+  },
+  destroyed () {
+    window.removeEventListener('resize', this.handleResize)
+  },
+  methods: {
+    handleResize () {
+      let el = document.getElementById('app')
+      el.className = ''
+      el.classList.add(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) || DEBUG ? 'mobile' : 'pc')
+      el.classList.add(window.innerHeight > window.innerWidth ? 'portrait' : 'landscape')
+    }
   }
 }
 </script>
