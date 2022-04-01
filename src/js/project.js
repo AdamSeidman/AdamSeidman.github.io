69acebf1102db46dfb761ed69936ac18f236bbfa
Adam <adamjseidman@gmail.com>
2022-04-01 05:19:05 -0400

    Updated Projects from Senior Year

--- src/js/project.js
+++ src/js/project.js
@@ -3,7 +3,7 @@ let i = 0
 class Project {
     constructor (name, route, content, technologies, imgName) {
         if (imgName === '') {
-            imgName = 'adam_seidman.jpg' // TODO remove this stuff when finished
+            imgName = 'adam_seidman.jpg'
         } else {
             imgName += '.png'
         }
@@ -27,6 +27,11 @@ const projects = [
         [
             'AOS', 'Bootstrap', 'Instagram API', 'JQuery'
         ], 'baja_logo'),
+    new Project('Basketball Shot Trainer', 'https://github.com/AdamSeidman/Basketball-Shot-Trainer',
+        'For my senior design project, I worked in a small group to create a basketball shot trainer for the Unified Sports organization. The resulting device analyzes body motion through a camera and a wearable IMU.',
+        [
+            'Adafruit Feather', 'Bluetooth', 'C', 'KiCad', 'Swift'
+        ], 'shot_trainer'),
     new Project('Bluetooth Robot', '',
         'A small robot that ran on a TI microcontroller that was given commands from a nearby tablet. This project was a collaboration with the Computer Engineering department at RIT.',
         [
@@ -47,6 +52,11 @@ const projects = [
         [
             'Arduino', 'C++', 'Sensors'
         ], 'line-follow'),
+    new Project('Custom Electromagnet', '', 
+        'I designed and manufactured an electromagnet to go inside a front differential for my last RIT Baja vehicle.',
+        [
+            'EM Simulations', 'Soldering', 'SolidWorks'
+        ], 'e-magnet'),
     new Project('Shifter Knob', '',
         'After my old shifter started to deteriorate, I decided it was time to make a custom one. I 3D printed a prototype and drove around with it for a few days. I then used several resources to make the real thing.',
         [
@@ -66,7 +76,12 @@ const projects = [
         'That\'s this website! I created this site in July/August 2020 in my free time between classes. Recently, I have been working with 3D CSS to create this project carousel. I wanted to try creating my own website with VueJS, and I am proud of the way it turned out.',
         [
             '3D CSS', 'Github-Pages', 'Sass', 'VS Code', 'VueJS'
-        ], 'vuejs')
+        ], 'vuejs'),
+    new Project('Stackable DAQ', 'https://github.com/AdamSeidman/SDAQ',
+        'Leading a small team, I created a new Raspberry Pi-based data acquisition system to be used for RIT Baja. These new technologies will/are being used to replace most of the teams\' outdated hardware.',
+        [
+            'C', 'I2C', 'KiCad', 'Python', 'Raspbian', 'Tkinter'
+        ], 'rpi')
 ]
 
 export default projects
