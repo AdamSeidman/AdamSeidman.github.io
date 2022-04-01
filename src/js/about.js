95ada0760fccdab7e8ee47d38e02643ce41902f3
Adam <adamjseidman@gmail.com>
2022-04-01 08:03:01 -0400

    Update about.js

--- src/js/about.js
+++ src/js/about.js
@@ -5,8 +5,8 @@ var response = {
     Institute of Technology. At RIT, I study Computer Engineering, and I am on an accelerated \
     track to get my bachelors and masters by the end of the year. I have also \
     completed an immersion in public policy. I am heavily involved with our school's Baja \
-    SAE team, where I design electrical projects for our vehicle. For two years, \
-    I have even served on the team's executive board. I have always loved everything about \
+    SAE team, where I design electrical projects for our vehicle. For the third year, \
+    I am even serving on the team's executive board. I have always loved everything about \
     both engineering and math. I enjoy learning about these skills, getting to use creativity, \
     and solving problems. When I am not at RIT, some of my favorite pastimes include chess, camping, \
     reading, and puzzles."
