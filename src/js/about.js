4319a206d4362b19f27ab391ca9f75269c75093f
Adam <adamjseidman@gmail.com>
2022-06-15 15:34:17 -0400

    Small Working Changes

--- src/js/about.js
+++ src/js/about.js
@@ -4,7 +4,7 @@ var response = {
     content: "I am a recent graduate from the Rochester Institute of Technology. \
     At RIT, I studied Computer Engineering and also completed an immersion in public \
     policy. I was heavily involved with RIT's Baja SAE team, where I designed many electrical \
-    projects for our vehicles. For three years, I even served on the team's executive board. \
+    projects for our vehicles. For three years, I served on the team's executive board. \
     I have always loved everything about both engineering and math. I enjoy learning about \
     these skills, getting to use creativity, and solving problems. Some of my favorite pastimes \
     include chess, camping, reading, and puzzles."
